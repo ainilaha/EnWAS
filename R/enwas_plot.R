ccbe2917dff2b3e0a47af38425aeaf34dd93928a
Laha Ale <ainilaha@gmail.com>
2022-09-08 22:38:38 -0400

    add bin plots and first vignettes

--- R/enwas_plot.R
+++ R/enwas_plot.R
@@ -1,5 +1,7 @@
 
 
+
+
 #' Forest Plot for single model
 #'
 #' @param xwas_result the EnWAS result data frame
@@ -20,23 +22,23 @@ forest_plot <- function(xwas_result) {
     geom_point(size = 2) +
     geom_errorbar(aes(ymin = lower, ymax = upper),
                   width = 0.5,
-                  cex = 1)+
-  geom_hline(yintercept = 0, linetype = 'dashed') +
-  geom_rect(
-    aes(
-      xmin = xmin,
-      xmax = xmax,
-      ymin = -Inf,
-      ymax = +Inf,
-      fill = factor(col)
-    ),
-    color = 'black',
-    alpha = 0.3
-  ) +
-  scale_fill_manual(values = c('white', 'grey78'), guide = 'none') +
-  coord_flip() +  # flip coordinates (puts labels on y axis)
-  xlab("Exposures") + ylab("Estimate (95% CI)") +
-  theme_bw()  # use a white background
+                  cex = 1) +
+    geom_hline(yintercept = 0, linetype = 'dashed') +
+    geom_rect(
+      aes(
+        xmin = xmin,
+        xmax = xmax,
+        ymin = -Inf,
+        ymax = +Inf,
+        fill = factor(col)
+      ),
+      color = 'black',
+      alpha = 0.3
+    ) +
+    scale_fill_manual(values = c('white', 'grey78'), guide = 'none') +
+    coord_flip() +  # flip coordinates (puts labels on y axis)
+    xlab("Exposures") + ylab("Estimate (95% CI)") +
+    theme_bw()  # use a white background
 }
 
 
@@ -44,7 +46,7 @@ forest_plot <- function(xwas_result) {
 
 
 
-#' Title
+#' Plot Forest for Muliple Models
 #'
 #' @param xwas_result_list list of the EnWAS result data frames
 #'
@@ -72,7 +74,7 @@ forest_plot_mult <- function(xwas_result_list) {
       position = position_dodge(width = 1),
       width = 0.5,
       cex = 1
-    )+
+    ) +
     geom_hline(yintercept = 0, linetype = 'dashed') +
     geom_rect(
       data = tem_df,
@@ -91,3 +93,81 @@ forest_plot_mult <- function(xwas_result_list) {
     xlab("Exposures") + ylab("Estimate (95% CI)") +
     theme_bw()  # use a white background
 }
+
+
+
+#' Make Bins for y values based on x
+#'
+#' @param x x values
+#' @param y y values
+#' @param nbin expected number of data point in each bin
+#'
+#' @return a data frame with binned data frame
+#' @export
+#'
+#' @examples make_bins(x,y,200)
+make_bins <- function(x, y, nbin) {
+  if (nbin == 0)
+    nbin <- floor(sqrt(length(x)))
+
+  df <-
+    data.frame(y = y,
+               breaks = Hmisc::cut2(x, m = nbin, levels.mean = TRUE))
+  df <- df |>
+    dplyr::group_by(breaks) |>
+    dplyr::summarize(mean = mean(y),
+                     std = sd(y),
+                     cnt = dplyr::n()) |> dplyr::mutate(y_min = mean - 1.96 * std /
+                                                   sqrt(cnt),
+                                                 y_max = mean + 1.96 * std / sqrt(cnt))
+
+  df$breaks <- as.numeric(as.character(df$breaks))
+
+  df
+
+}
+
+#' Plot multiple Bins
+#'
+#' @param df_list a list contains data frames
+#' @param xlab x-axis label
+#' @param ylab x-axis label
+#' @param is_facet Boolean flag to decide create facets
+#'
+#' @return plots binned data
+#' @export
+#'
+#' @examples plot_bins2(df_list)
+plot_bins2 <-
+  function(df_list,
+           xlab = "x",
+           ylab = "Binned y",
+           is_facet = TRUE) {
+    df <- do.call("rbind", df_list)
+    df$Model <-
+      rep(names(df_list), each = nrow(df_list[[1]]))
+
+    g <-
+      ggplot(df, aes(breaks, mean, color = Model)) + geom_point() +
+      geom_errorbar(aes(ymin = y_min, ymax = y_max)) +
+      geom_smooth(aes(breaks, mean), se = FALSE) +
+      scale_color_manual(
+        values = c(
+          "#E69F00",
+          "#56B4E9",
+          "#009E73",
+          "#F0E442",
+          "#0072B2",
+          "#D55E00",
+          "#CC79A7"
+        )
+      ) +
+      ylab(ylab) + xlab(xlab) +
+      theme_minimal()
+    if (is_facet) {
+      g <- g + facet_grid(cols = vars(Model))
+    }
+
+    g
+
+  }
